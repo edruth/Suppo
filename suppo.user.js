c1ea9bb51fb2e2d3b6c9c642a6268e31a77d9991
edruth <edruth@mailinator.com>
2016-02-09 16:56:59 +0200

    Adding the script itself

---
+++ suppo.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Suppo
+// @namespace   Suppo
+// @description Script to assist Yotpo support in daily tasks
+// @include     *
+// @version     0.0.1
+// @grant       none
+// ==/UserScript==
+
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//code.jquery.com/jquery-2.2.0.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+function main() {
+	console.log("version 0.0.1");
+}
+
+addJQuery(main);
